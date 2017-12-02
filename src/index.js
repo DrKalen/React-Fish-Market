97cbb347a923900515f7590d316128357ee4b55c
Kalen Hammann <kalen7@gmail.com>
2017-12-02 14:27:44 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// let's go!
+alert ('yeah!!!')
+
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker/>, document.querySelector('#main'));
