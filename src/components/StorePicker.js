0a833be33bc3de4fee55f30c9883dfdf10e372d7
Kalen Hammann <kalen7@gmail.com>
2017-12-03 09:44:17 -0500

    Added ref to pull storepicker choice from the page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore (event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        //first grab the text from the box
+        console.log(this.storeInput.value);
+        //second transition from url = / to /store/:storeId
+
+
+    }
+
     render() {
+        //This kind of comment is fine in React except inside JSX
         return (
-            //This kind of comment is fine in React except inside JSX
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
                 { /* Notice this is how to do a comment INSIDE JSX! */ }
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name" 
-                    default value={getFunName()} />
+                    defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
                 <button type="submit">Visit Store -></button>
             </form>
         )
