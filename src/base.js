f25ddcfbfcf9e504a736e1bcbe4dae0f523c147a
Kalen Hammann <kalen7@gmail.com>
2017-12-03 19:16:25 -0500

    Set up sync to Firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDpjHHoEal9ss_MZzZ9pgFk9gdHQjJpYEw",
+    authDomain: "catch-of-the-day-3ca55.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-3ca55.firebaseio.com",
+});
+
+export default base;
